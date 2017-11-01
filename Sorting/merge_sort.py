cd1c6d117feef7ee039abc910103803c2b945961
LAPTOP-FCV7JBCF\Charlie <haoen95@gmail.com>
2017-10-31 22:58:15 -0400

    Added MergeSort

---
+++ Sorting/merge_sort.py
@@ -0,0 +1,39 @@
+import random
+
+def merge(left, right):
+    l = 0
+    r = 0
+    array = []
+    while l < len(left) or r < len(right):
+        if l >= len(left):
+            array.extend(right[r:])
+            break
+        elif r >= len(right):
+            array.extend(left[l:])
+            break
+        if left[l] > right[r]:
+            array.append(right[r])
+            r += 1
+        else:
+            array.append(left[l])
+            l += 1
+    return array
+
+def merge_sort(array):
+    # Base case is when the array is of size 0 (empty) or 1
+    # Base case is already sorted so just return
+    if len(array) < 2:
+        return array
+    # Divide it into a left and right side
+    mid = len(array) / 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    return merge(left, right)
+
+if __name__=="__main__":
+    for _ in xrange(10):
+        array = []
+        for _ in xrange(20):
+            array.append(random.randrange(0, 100))
+        assert merge_sort(array) == sorted(array)
+    
